245a2a3f7e5eec5b213767ce188a936534d4aa7d
Roman Grebennikov <grebennikov.roman@gmail.com>
2016-06-22 13:46:45 +0300

    more verbose http logging

--- src/main/scala/io/findify/databricks/calls/ApiCall.scala
+++ src/main/scala/io/findify/databricks/calls/ApiCall.scala
@@ -6,7 +6,7 @@ import akka.http.scaladsl.model.headers.{Authorization, BasicHttpCredentials}
 import akka.http.scaladsl.model.{HttpHeader, HttpMethods, HttpRequest, Uri}
 import akka.stream.ActorMaterializer
 import akka.util.ByteString
-import com.typesafe.scalalogging.LazyLogging
+import com.typesafe.scalalogging.slf4j.LazyLogging
 import io.findify.databricks.Auth
 import spray.json._
 
@@ -21,19 +21,23 @@ abstract class ApiCall(endpoint:String, client:HttpExt, auth:Auth) extends LazyL
   import system.dispatcher
 
   protected def get(method:String, params:Map[String,String] = Map()) = {
-    logger.debug(s"Sending GET request to $endpoint/put, params = $params")
+    logger.debug(s"Sending GET request to $endpoint/$method, params = $params")
     client.singleRequest(HttpRequest(
       uri = Uri(s"$endpoint/$method").withQuery(Query(params)),
       method = HttpMethods.GET,
       headers = List[HttpHeader](Authorization(BasicHttpCredentials(auth.username, auth.password)))
     )).flatMap( _.entity.dataBytes.runFold(ByteString(""))(_ ++ _))
+      .map(result => {
+        logger.debug(s"Got response: $result")
+        result
+      })
   }
   protected def getJson(method:String, params:Map[String,String] = Map()):Future[JsValue] = {
     get(method, params).map(_.utf8String.parseJson)
   }
 
   protected def postJson(method:String, data:JsValue):Future[JsValue] = {
-    logger.debug(s"Sending POST request to $endpoint/put")
+    logger.debug(s"Sending POST request to $endpoint/$method")
     client.singleRequest(HttpRequest(
       uri = s"$endpoint/$method",
       method = HttpMethods.POST,
@@ -41,6 +45,10 @@ abstract class ApiCall(endpoint:String, client:HttpExt, auth:Auth) extends LazyL
       headers = List[HttpHeader](Authorization(BasicHttpCredentials(auth.username, auth.password)))
     ))
       .map( _.entity.dataBytes.runFold(ByteString(""))(_ ++ _))
-      .flatMap(_.map(_.utf8String.parseJson))
+      .flatMap(_.map(_.utf8String))
+      .map(js => {
+        logger.debug(s"Got response: $js")
+        js.parseJson
+      })
   }
 }
